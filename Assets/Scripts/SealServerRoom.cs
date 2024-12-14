7fdfb418463b80b6403007c3b9e34910e03e4c12
Yousif <yousifhazardous@gmail.com>
2024-12-14 18:33:30 +0300

    Add Post Processing & Atmospheric Sounds

--- Assets/Scripts/SealServerRoom.cs
+++ Assets/Scripts/SealServerRoom.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class SealServerRoom : MonoBehaviour
 {
     public GameObject myDoor;
+    public GameObject myDoor2;
+    public GameObject myDoor3;
 
     private void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.tag == "Player")
         {
             myDoor.SetActive(true);
+            myDoor2.SetActive(true);
+            myDoor3.SetActive(true);
         }
     }
 }
